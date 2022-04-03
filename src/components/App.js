93630603eb3b088e83d13e1acae87817b58f6578
Dawidpios <dawid.pios00@gmail.com>
2022-04-03 18:34:37 +0200

    creating playground

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+
+
+import styles from '../styles/componentsStyle/App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+     
+    </div>
+  );
+}
+
+export default App;
