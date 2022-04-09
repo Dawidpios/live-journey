917f4fcf1feb129b6377a70b2ee83ae401b8d144
Dawidpios <dawid.pios00@gmail.com>
2022-04-09 14:42:16 +0200

    add test

--- src/pages/RegisterForm.js
+++ src/pages/RegisterForm.js
@@ -6,6 +6,8 @@ const RegisterForm = () => {
         <form>
             <label>
                 <input></input>
+                <input type="password"></input>
+                <input type="email"></input>
             </label>
         </form>
     </div>
